4c2aa52d281af4dd392a88902c0765854931d19e
haochenchn <haochenchn@163.com>
2020-09-14 13:41:51 +0800

    获取sso服务器自定义返回内容

--- sso-client-spring/src/main/java/com/aaron/cas/controller/UserController.java
+++ sso-client-spring/src/main/java/com/aaron/cas/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.aaron.cas.controller;
 
 import com.aaron.cas.service.IUserService;
+import org.jasig.cas.client.authentication.AttributePrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 
 @Controller
@@ -18,6 +22,16 @@ public class UserController {
     public String insert(String username){
         userService.insert(username);
         return "result";
+    }
+
+    @RequestMapping("test")
+    @ResponseBody
+    public Object test(HttpServletRequest request){
+        AttributePrincipal principal = (AttributePrincipal)request.getUserPrincipal();
+        String name = principal.getName();
+        Map attributes = principal.getAttributes();
+        attributes.put("name", name);
+        return attributes;
     }
 
     /**

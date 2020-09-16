b0ec90d04452145c73246eb3262d77dc400a913e
haochenchn <haochenchn@163.com>
2020-09-17 00:59:33 +0800

    手写 springboot 的 cas-starter

--- cas-client-spring-boot-starter/src/main/java/com/hx/cas/client/configuration/CasClientConfigProperties.java
+++ cas-client-spring-boot-starter/src/main/java/com/hx/cas/client/configuration/CasClientConfigProperties.java
@@ -12,33 +12,30 @@ import javax.validation.constraints.NotNull;
 @ConfigurationProperties(prefix = "cas", ignoreUnknownFields = false)
 public class CasClientConfigProperties {
     /**
-     * CAS server URL E.g. https://example.com/cas or https://cas.example. Required.
      * CAS 服务端 url，不能为空
      */
     @NotNull
     private String serverUrlPrefix;
 
     /**
-     * CAS server login URL E.g. https://example.com/cas/login or https://cas.example/login. Required.
-     * CAS 服务端登录地址，面的链接加上/login 该参数不能为空
+     * CAS 服务端登录地址，serverUrlPrefix加上/login，不能为空
      */
     @NotNull
     private String serverLoginUrl;
 
     /**
-     * CAS-protected client application host URL E.g. https://myclient.example.com Required.
-     * 当前客户端的地址
+     * 当前客户端的地址，不能为空
      */
     @NotNull
     private String serverName;
 
     /**
-     * 忽略规则,访问那些地址 不需要登录
+     * 忽略规则,访问那些地址不需要登录，如 /js/*|/images/*|/css/*
      */
     private String ignorePattern;
 
     /**
-     * 自定义UrlPatternMatcherStrategy验证
+     * 自定义UrlPatternMatcherStrategy验证，必须全类名
      */
     private String ignoreUrlPatternType;
 

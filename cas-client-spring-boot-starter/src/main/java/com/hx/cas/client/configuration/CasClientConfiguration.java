260c4b9db401828ef415483f6bfee122ad5198a0
haochenchn <haochenchn@163.com>
2020-09-18 14:52:01 +0800

    优化该springboot starter

--- cas-client-spring-boot-starter/src/main/java/com/hx/cas/client/configuration/CasClientConfiguration.java
+++ cas-client-spring-boot-starter/src/main/java/com/hx/cas/client/configuration/CasClientConfiguration.java
@@ -30,7 +30,7 @@ public class CasClientConfiguration {
     CasClientConfigProperties configProps;
 
     /**
-     * 添加监听器
+     * 该监听器用于实现单点登出功能，session失效监听器
      * @return
      */
     @Bean
@@ -42,8 +42,9 @@ public class CasClientConfiguration {
     }
 
     /**
-     * 配置登出过滤器
-     * @return
+     * 该过滤器用于实现单点登出功能，当一个系统登出时，cas服务端会通知，各个应
+     * 用进行进行退出操作，该过滤器就是用来接收cas回调的请求，如果是前后端分离
+     * 应用，需要重写SingleSignOutFilter过滤器，按自已的业务规则去处理
      */
     @Bean
     public FilterRegistrationBean filterSingleRegistration() {
@@ -53,27 +54,6 @@ public class CasClientConfiguration {
         registration.addUrlPatterns("/*");
         Map<String,String>  initParameters = new HashMap<String, String>();
         initParameters.put("casServerUrlPrefix", configProps.getServerUrlPrefix());
-        registration.setInitParameters(initParameters);
-        // 设定加载的顺序
-        registration.setOrder(1);
-        return registration;
-    }
-
-    /**
-     * 配置过滤验证器 这里用的是Cas30ProxyReceivingTicketValidationFilter
-     * 该过滤器负责对Ticket的校验工作
-     * @return
-     */
-    @Bean
-    public FilterRegistrationBean filterValidationRegistration() {
-        final FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(new Cas30ProxyReceivingTicketValidationFilter());
-        // 设定匹配的路径
-        registration.addUrlPatterns("/*");
-        Map<String,String> initParameters = new HashMap();
-        initParameters.put("casServerUrlPrefix", configProps.getServerUrlPrefix());
-        initParameters.put("serverName", configProps.getServerName());
-        initParameters.put("useSession", "true");
         registration.setInitParameters(initParameters);
         // 设定加载的顺序
         registration.setOrder(2);
@@ -113,6 +93,27 @@ public class CasClientConfiguration {
     }
 
     /**
+     * 配置过滤验证器 这里用的是Cas30ProxyReceivingTicketValidationFilter
+     * 该过滤器负责对Ticket的校验工作
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean filterValidationRegistration() {
+        final FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new Cas30ProxyReceivingTicketValidationFilter());
+        // 设定匹配的路径
+        registration.addUrlPatterns("/*");
+        Map<String,String> initParameters = new HashMap();
+        initParameters.put("casServerUrlPrefix", configProps.getServerUrlPrefix());
+        initParameters.put("serverName", configProps.getServerName());
+        initParameters.put("useSession", "true");
+        registration.setInitParameters(initParameters);
+        // 设定加载的顺序
+        registration.setOrder(4);
+        return registration;
+    }
+
+    /**
      * request wraper过滤器
      * 该过滤器负责实现HttpServletRequest请求的包裹，
      * 比如允许开发者通过HttpServletRequest的getRemoteUser()方法获得SSO登录用户的登录名
@@ -125,7 +126,7 @@ public class CasClientConfiguration {
         // 设定匹配的路径
         registration.addUrlPatterns("/*");
         // 设定加载的顺序
-        registration.setOrder(4);
+        registration.setOrder(5);
         return registration;
     }
 
@@ -133,6 +134,7 @@ public class CasClientConfiguration {
      * 该过滤器使得开发者可以通过org.jasig.cas.client.util.AssertionHolder来获取用户的登录名。
      * 比如AssertionHolder.getAssertion().getPrincipal().getName()
      * 或者request.getUserPrincipal().getName()
+     * 这个类把Assertion信息放在ThreadLocal变量中，这样应用程序不在web层也能够获取到当前登录信息
      * @return
      */
     @Bean
@@ -142,7 +144,7 @@ public class CasClientConfiguration {
         // 设定匹配的路径
         registration.addUrlPatterns("/*");
         // 设定加载的顺序
-        registration.setOrder(5);
+        registration.setOrder(6);
         return registration;
     }
 
